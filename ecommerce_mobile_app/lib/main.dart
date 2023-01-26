79c8e931e6cb57876fb59c05e7193209a7413e38
apple <apple@apples-MacBook-Pro.local>
2023-01-26 18:50:54 +0500

    updated

--- ecommerce_mobile_app/lib/main.dart
+++ ecommerce_mobile_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_mobile_app/routes.dart';
 import 'package:flutter/material.dart';
+
 import './screens/splash/splash_screen.dart';
 import './theme.dart';
 import './web-scroll-behaviour.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
 4. git comment -m "description"
 5. git branch -M BranchName
 6. git remote add origin reopUrl
-6. git -u origin BranchName
+6. git -u origin BranchNam
 */
 
 /*
@@ -43,4 +44,4 @@ Step-1 Step by step 3 commands to upload modified files/folders.
 → 2. git commit -m "description"
 → 3. git push -u origin branchName
 */
-//token_access --> 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   
+//token_access --> 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
